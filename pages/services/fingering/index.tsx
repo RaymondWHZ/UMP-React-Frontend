2d3989ef8074bfe2cbdafb79120112cb610ab343
Sarah Wang <sarahprwang@gmail.com>
2022-08-12 21:33:29 -0500

    reminders on both fingering and sightreading

--- pages/services/fingering/index.tsx
+++ pages/services/fingering/index.tsx
@@ -121,6 +121,7 @@ const FileDropZone = ({ onDrop }: FileDropZoneProps) => {
       ) : (
         <>
           <Text fontSize="30px" color="white">Drag Your File Here or Browse</Text>
+          <Text fontSize="25px" color="white">Please Only Upload Digital Printed Music Score</Text>
         </>
       )}
     </VStack>
